109d7c751cb22c3e8087b11d3682a26228b267d0
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-08-23 00:30:57 +0300

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Николай Никитин on 23.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
