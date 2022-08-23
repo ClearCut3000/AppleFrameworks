9488e573d5152e21ee443f15cc77df57c981a803
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-08-24 00:06:18 +0300

    Added SafariView

--- AppleFrameworks/FrameworkDetailView.swift
+++ AppleFrameworks/FrameworkDetailView.swift
@@ -11,6 +11,7 @@ struct FrameworkDetailView: View {
   var framework: Framework
 
   @Binding var isShowingDetailView: Bool
+  @State private var isShowingSafariView = false
 
   var body: some View {
     VStack {
@@ -33,12 +34,14 @@ struct FrameworkDetailView: View {
         .padding()
       Spacer()
       Button {
-
+        isShowingSafariView = true
       } label: {
         AFButton(title: "Learn more")
       }
     }
-
+    .fullScreenCover(isPresented: $isShowingSafariView) {
+      SafariView(url: URL(string: framework.urlString) ?? URL(string: "https://en.wikipedia.org/wiki/HTTP_404")!)
+    }
   }
 }
 
